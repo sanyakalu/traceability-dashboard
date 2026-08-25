c45d2720367e746fb6c4aa8358d33a7222271713
sanyakalu <sanya.kaluarachchi@philips.com>
2026-08-25 09:52:56 -0400

    Update SQL query to use version '5.0.0'

--- fetch_data.py
+++ fetch_data.py
@@ -46,8 +46,7 @@ conn = snowflake.connector.connect(
 
 cur = conn.cursor()
 cur.execute(
-    'SELECT * FROM TRACEABILITY_MATRIX WHERE REQMT_FOR_VERSION = %s',
-    ('5.0',)
+    "SELECT * FROM TRACEABILITY_MATRIX WHERE REQMT_FOR_VERSION = '5.0.0'"
 )
 cols = [COLUMN_MAP[d[0]] for d in cur.description]
 
